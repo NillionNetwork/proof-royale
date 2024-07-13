9e48f060d1d1e7df840638c7f189bd78c05d8f0b
Nicholas Wehr <github@biochx.com>
2024-07-13 14:43:06 +0200

    wip

--- gamer-stats/src/App.js
+++ gamer-stats/src/App.js
@@ -7,14 +7,14 @@ import {
   ListItem,
   Text,
 } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, useLocation } from "react-router-dom";
 import axios from "axios";
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
 };
 
-const App = () => {
+const GamerStats = () => {
   const [userInfo, setUserInfo] = useState(null);
   const query = useQuery();
   const username = query.get("username");
@@ -36,36 +36,43 @@ const App = () => {
   }
 
   return (
-    <ChakraProvider>
-      <Box
-        maxW="md"
-        mx="auto"
-        mt="10"
-        p="5"
-        borderWidth="1px"
-        borderRadius="lg"
-      >
-        <Heading mb="4" textAlign="center">Gamer Stats</Heading>
-        <Text fontSize="xl">Username: {userInfo.user}</Text>
-        <Text fontSize="xl">Score: {userInfo.score}</Text>
-        <Heading size="md" mt="4">Achievements</Heading>
-        <Text fontSize="md">completed {userInfo.percentage}</Text>
-        <List spacing={3} mt="2">
-          {userInfo.achievements.map((achievement, index) => (
-            <ListItem
-              key={index}
-              border="1px"
-              borderColor="gray.200"
-              p="2"
-              borderRadius="md"
-            >
-              {achievement}
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    </ChakraProvider>
+    <Box
+      maxW="md"
+      mx="auto"
+      mt="10"
+      p="5"
+      borderWidth="1px"
+      borderRadius="lg"
+    >
+      <Heading mb="4" textAlign="center">Gamer Stats</Heading>
+      <Text fontSize="xl">Username: {userInfo.user}</Text>
+      <Text fontSize="xl">Score: {userInfo.score}</Text>
+      <Heading size="md" mt="4">Achievements</Heading>
+      <Text fontSize="md">completed {userInfo.percentage}</Text>
+      <List spacing={3} mt="2">
+        {userInfo.achievements.map((achievement, index) => (
+          <ListItem
+            key={index}
+            border="1px"
+            borderColor="gray.200"
+            p="2"
+            borderRadius="md"
+          >
+            {achievement}
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 };
 
+const App = () => {
+  return (
+    <ChakraProvider>
+      <Router>
+        <Route path="/" component={GamerStats} />
+      </Router>
+    </ChakraProvider>
+  );
+};
 export default App;
